854251cd789ce2fe74a16960f78f44b9bf3c92cb
edenderrick47 <38224691+edenderrick47@users.noreply.github.com>
2022-01-12 13:39:47 +0800

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://springscommunityempowermentfoundation.org/api',
+  url: 'http://nxx1ljtyfiq4ax0m3262.cleaver.rocks/api',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '8c1620de6ce99ac1afbfffe73e96807b'),
